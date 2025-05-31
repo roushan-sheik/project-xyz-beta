861219b67aab3000b0b61a5d86185dc272dc41e0
roushan-sheik <roushansheik@gmail.com>
2025-05-31 13:36:48 +0600

    [refactor]: design system

---
+++ src/shared/SectionContainer.tsx
@@ -0,0 +1,39 @@
+import type { ReactNode, HTMLAttributes } from "react";
+import React from "react";
+
+type SectionContainerProps = {
+  children: ReactNode;
+  containerWrapperProps?: HTMLAttributes<HTMLDivElement>;
+  containerInnerProps?: HTMLAttributes<HTMLDivElement>;
+};
+
+const SectionContainer = ({
+  children,
+  containerInnerProps,
+  containerWrapperProps,
+}: SectionContainerProps) => {
+  const { className: wrapperClassName, ...restWrapperProps } =
+    containerWrapperProps || {};
+  const { className: innerClassName, ...restInnerProps } =
+    containerInnerProps || {};
+
+  return (
+    <div
+      className={`flex items-center justify-center max-w-full px-4 w-full ${
+        wrapperClassName || ""
+      }`}
+      {...restWrapperProps}
+    >
+      <div
+        className={`flex flex-col mx-auto xl:max-w-[85%] px-5 w-full ${
+          innerClassName || ""
+        }`}
+        {...restInnerProps}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default SectionContainer;
