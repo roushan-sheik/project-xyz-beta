3827e7b933c7a53283c135f0165dc7a26380a1ea
mdalamin-jbc <jbc.alaminhaque@gmail.com>
2025-06-19 15:52:27 +0600

    [fix]: Redirect problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,26 @@
 "use client";
 
 import SectionContainer from "@/shared/SectionContainer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { menuItems } from "@/constants/home/home";
 import { MenuItem } from "@/types/home/types";
 import Cart from "@/components/ui/Cart";
 import { useUser } from "@/context/AuthContext";
 import Link from "next/link";
 import Menu from "@/components/home/Menu";
+import { useRouter } from "next/navigation";
 
 const MainComponent = () => {
-  const [loading, setLoading] = useState(false);
-  const { user } = useUser();
-  console.log({ user });
+  const { user, isLoading } = useUser();
+  const router = useRouter();
 
-  if (loading) {
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.push("/login");
+    }
+  }, [user, isLoading, router]);
+
+  if (isLoading) {
     return (
       <div className="main_gradient_bg min-h-screen flex items-center justify-center">
         <h2 className="text-white text-heading2">読み込み中...</h2>
@@ -26,17 +32,15 @@ const MainComponent = () => {
     <div className="main_gradient_bg">
       <Menu text="Menu" />
       <SectionContainer>
-        <div className="  py-12   md:py-16  lg:py-20">
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-4 w-full  mx-auto">
-            {menuItems.map((item: MenuItem) => {
-              return (
-                <div key={item.id}>
-                  <Link href={item.path}>
-                    <Cart item={item} />
-                  </Link>
-                </div>
-              );
-            })}
+        <div className="py-12 md:py-16 lg:py-20">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-4 w-full mx-auto">
+            {menuItems.map((item: MenuItem) => (
+              <div key={item.id}>
+                <Link href={item.path}>
+                  <Cart item={item} />
+                </Link>
+              </div>
+            ))}
           </div>
         </div>
       </SectionContainer>
