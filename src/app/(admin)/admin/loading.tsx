2a593d567789609e58c6c61bd2b3597518a20bc3
roushan-sheik <roushansheik@gmail.com>
2025-06-25 15:47:43 +0600

    [style]: Fix video request page responsive

---
+++ src/app/(admin)/admin/loading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className=" flex justify-center items-center min-h-screen w-full">
+      <div className=" min-h-screen flex items-center justify-center">
+        <h2 className="text-black text-heading2">読み込み中...</h2>
+      </div>
+    </div>
+  );
+};
+
+export default loading;
